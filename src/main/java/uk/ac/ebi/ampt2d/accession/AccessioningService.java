9f0e909f5995376eb611d967ecc739bd1a9ae878
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-03-13 13:57:40 +0000

    Added license files and some comments to the code

--- src/main/java/uk/ac/ebi/ampt2d/accession/AccessioningService.java
+++ src/main/java/uk/ac/ebi/ampt2d/accession/AccessioningService.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.ebi.ampt2d.accession;
 
 import java.util.HashMap;
@@ -16,15 +31,21 @@ public class AccessioningService<T> {
     }
 
     public Map<T, String> createAccessions(List<T> objects) {
-        Map<T, String> accessions = new HashMap<T, String>();
+        Map<T, String> accessions = new HashMap<>();
+
         for (T object : objects) {
+            // look for an existing accession for this object
             String accession = accessionRepository.get(object);
+
             if (accession == null) {
+                // if there is no previous accession for the object, create a new one and store it in the repository
                 accession = accessionGenerator.get(object);
                 accessionRepository.add(object, accession);
             }
+
             accessions.put(object, accession);
         }
+
         return accessions;
     }
 }
