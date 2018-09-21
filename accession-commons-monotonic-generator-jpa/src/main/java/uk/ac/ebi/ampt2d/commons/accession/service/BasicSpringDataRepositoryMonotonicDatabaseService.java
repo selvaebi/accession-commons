439aa8b59e45d9ce2e8592b9ec7dbe9adaaa86a9
jose miguel mut <jomutlo@gmail.com>
2018-09-21 11:19:09 +0100

    add BasicSpringDataRepositoryMonotonicDatabaseService

---
+++ accession-commons-monotonic-generator-jpa/src/main/java/uk/ac/ebi/ampt2d/commons/accession/service/BasicSpringDataRepositoryMonotonicDatabaseService.java
@@ -0,0 +1,81 @@
+/*
+ *
+ * Copyright 2018 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package uk.ac.ebi.ampt2d.commons.accession.service;
+
+import uk.ac.ebi.ampt2d.commons.accession.core.DatabaseService;
+import uk.ac.ebi.ampt2d.commons.accession.core.exceptions.AccessionDeprecatedException;
+import uk.ac.ebi.ampt2d.commons.accession.core.exceptions.AccessionDoesNotExistException;
+import uk.ac.ebi.ampt2d.commons.accession.core.exceptions.AccessionMergedException;
+import uk.ac.ebi.ampt2d.commons.accession.core.exceptions.HashAlreadyExistsException;
+import uk.ac.ebi.ampt2d.commons.accession.core.models.AccessionVersionsWrapper;
+import uk.ac.ebi.ampt2d.commons.accession.core.models.AccessionWrapper;
+import uk.ac.ebi.ampt2d.commons.accession.core.models.EventType;
+import uk.ac.ebi.ampt2d.commons.accession.core.models.SaveResponse;
+import uk.ac.ebi.ampt2d.commons.accession.generators.monotonic.MonotonicRange;
+import uk.ac.ebi.ampt2d.commons.accession.persistence.jpa.monotonic.service.MonotonicDatabaseService;
+import uk.ac.ebi.ampt2d.commons.accession.persistence.models.IAccessionedObject;
+import uk.ac.ebi.ampt2d.commons.accession.persistence.repositories.IAccessionedObjectRepository;
+import uk.ac.ebi.ampt2d.commons.accession.persistence.services.BasicSpringDataRepositoryDatabaseService;
+import uk.ac.ebi.ampt2d.commons.accession.persistence.services.InactiveAccessionService;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Basic implementation of {@link MonotonicDatabaseService} that requires a Spring Data repository that extends
+ * {@link IAccessionedObjectRepository}, a function to generate the entities from the triple model/hash/accession, a function
+ * to get the accession from the entity and a function to get the hashed representation of the message from the entity.
+ *
+ * Reuses implementation for most methods from {@link BasicSpringDataRepositoryDatabaseService}.
+ *
+ * @param <MODEL>
+ * @param <ACCESSION_ENTITY>
+ */
+public class BasicSpringDataRepositoryMonotonicDatabaseService<
+        MODEL,
+        ACCESSION_ENTITY extends IAccessionedObject<MODEL, String, Long>>
+        extends BasicSpringDataRepositoryDatabaseService<MODEL, Long, ACCESSION_ENTITY>
+        implements MonotonicDatabaseService<MODEL, String> {
+
+    private final IAccessionedObjectRepository<ACCESSION_ENTITY, Long> repository;
+
+    private final Function<AccessionWrapper<MODEL, String, Long>, ACCESSION_ENTITY> toEntityFunction;
+
+    private final InactiveAccessionService<MODEL, Long, ACCESSION_ENTITY> inactiveAccessionService;
+
+    public BasicSpringDataRepositoryMonotonicDatabaseService(
+            IAccessionedObjectRepository<ACCESSION_ENTITY, Long> repository,
+            Function<AccessionWrapper<MODEL, String, Long>, ACCESSION_ENTITY> toEntityFunction,
+            InactiveAccessionService<MODEL, Long, ACCESSION_ENTITY> inactiveAccessionService) {
+        super(repository, toEntityFunction, inactiveAccessionService);
+        this.repository = repository;
+        this.toEntityFunction = toEntityFunction;
+        this.inactiveAccessionService = inactiveAccessionService;
+    }
+
+    @Override
+    public long[] getAccessionsInRanges(Collection<MonotonicRange> ranges) {
+        return new long[0];
+    }
+}
